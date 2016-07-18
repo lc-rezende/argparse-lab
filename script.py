fe0934b9daa6130f70ba35822d3c4f1ffd16cf4c
Lucas Rezende <lrezende@b3d2a366.virtua.com.br>
2016-07-17 22:53:45 -0300

    updated

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 import sys
 import argparse
 
+INPUTFILE = ''
+OUTPUTFILE = ''
+
 args = argparse.ArgumentParser(description='')
 args.add_argument('-i', '--input', help='Youd should provide the name of the input file.', required=True)
 args.add_argument('-o', '--output', help='Your should provide the name of the output file.', required=True)
@@ -15,3 +18,13 @@ print '=========================================================================
 print args
 print '======================================================================================================='
 print ''
+
+INPUTFILE = args['input']
+OUTPUTFILE = args['output']
+
+print '======================================================================================================='
+print 'This is the input file name: %s ' % (INPUTFILE)
+print 'This is the output file name: %s ' % (OUTPUTFILE)
+print '======================================================================================================='
+print ''
+
